fb660654e4713621f851951eb63c8854b789d7fe
develaper <ibrahim.lachguer@gmail.com>
2020-11-08 19:23:18 +0100

    Adds FileValidator

--- app/models/emission_importer.rb
+++ app/models/emission_importer.rb
@@ -1,12 +1,19 @@
 require 'csv'
 
 class EmissionImporter
+  class InvalidFileException < StandardError; end
+
   # Creates a new Emission record in the db
   # for each row of the csv in the file_path.
   # params: file_path, String.
   #
   def import_from_csv(file_path)
-    CSV.foreach(Rails.root.to_s + file_path, headers: true, converters: :numeric) do |row|
+    begin
+      file_path = FileValidator.new(file_path).path
+    rescue FileValidator::FileValidatorException => e
+      handle_invalid_file_exception(e)
+    end
+    CSV.foreach(file_path, headers: true, converters: :numeric) do |row|
       row_data = row.to_hash.transform_keys! { |key| key.to_s.downcase }
       values_by_year = row_data.select { |key, value| numeric?(key) }
       Emission.create(country: row_data["country"], sector: row_data["sector"], parent_sector: row_data["parent ector"], values_by_year: values_by_year)
@@ -19,5 +26,14 @@ class EmissionImporter
   #
   def numeric?(word)
     Float(word) != nil rescue false
+  end
+  # Loggs exception information.
+  # Raises InvalidFileException.
+  # params: e, error.
+  #
+  def handle_invalid_file_exception(e)
+    Rails.logger.error e.message
+    Rails.logger.error e.backtrace.join("\n")
+    raise EmissionImporter::InvalidFileException.new(e.message)
   end
 end
